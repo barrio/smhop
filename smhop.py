5dd0e6715d2287d9e177ddc632a156068533d51b
Marco Heins <marco.heins@posteo.de>
2025-02-09 20:52:29 +0100

    attach to chromium

--- smhop.py
+++ smhop.py
@@ -9,11 +9,12 @@ import IPython
 import nest_asyncio
 import parsel
 from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
 
 nest_asyncio.apply()
 
 
-url = "https://www.rewe.de/angebote/goettingen/241057/rewe-markt-reinhaeuser-landstr-177/"
+rewe_angebote_url = "https://www.rewe.de/angebote/goettingen/241057/rewe-markt-reinhaeuser-landstr-177/"
 cookie_button = "#uc-center-container > div.sc-eBMEME.ixkACg > div > div.sc-jsJBEP.iXSECa > div > button:nth-child(2)"
 categories_button_css = string.Template("li.sos-navigation__categories-item:nth-child($num) > a:nth-child(1)")
 
@@ -37,10 +38,15 @@ def scroll(page, js=False):
             break
 
 
-def create_page(playwright_context):
-    browser = playwright_context.firefox.launch(headless=False)
-    context = browser.new_context(viewport={'width': 1920, 'height': 1080})
-    return context.new_page()
+def create_page(playwright_context, attach=False):
+    if attach:
+        browser = playwright_context.chromium.connect_over_cdp("http://localhost:9222")
+        default_context = browser.contexts[0]
+        return default_context.pages[0]
+    else:
+        browser = playwright_context.firefox.launch(headless=False)
+        context = browser.new_context(viewport={'width': 1920, 'height': 1080})
+        return context.new_page()
 
 
 def click_cookie_banner(page):
@@ -65,17 +71,25 @@ def get_products(html, css):
     ]
 
 
-def main():
+def rewe_angebote():
     with sync_playwright() as playwright_context:
         page = create_page(playwright_context)
-        page.goto(url)
+        page.goto(rewe_angebote_url)
         click_cookie_banner(page)
         cycle_categories(page)
         angebote = get_products(page.content(), "a")
-        #angebote = [product.replace(",", "", 1) for product in angebote]
+
         with open("rewe_angebote.txt", "w") as rewe_file:
             rewe_file.write("\n".join(angebote))
 
 
+def rewe_suche():
+    with sync_playwright() as playwright_context:
+        page = create_page(playwright_context, attach=True)
+        page.goto("https://www.rewe.de/produkte/ja-gouda-mittelalt-300g/6337252")
+        price = page.get_by_test_id("spd-product-price-value").inner_text().split(" €")[0]
+        pp(f"gouda-mittelalt-80g {price}")
+
+
 if __name__ == '__main__':
-    main()
+    rewe_suche()
