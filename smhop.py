ad2a764a2cdb3088905ec80a1b1dc447d6fcee7c
Marco Heins <marco.heins@posteo.de>
2025-02-09 16:03:42 +0100

    refactor: readability

--- smhop.py
+++ smhop.py
@@ -5,16 +5,17 @@ from pprint import pp
 import string
 import time
 
-from playwright.sync_api import sync_playwright
-import parsel
 import IPython
 import nest_asyncio
+import parsel
+from playwright.sync_api import sync_playwright
 
 nest_asyncio.apply()
 
 
-URL = "https://www.rewe.de/angebote/goettingen/241057/rewe-markt-reinhaeuser-landstr-177/"
-button_css = string.Template("li.sos-navigation__categories-item:nth-child($num) > a:nth-child(1)")
+url = "https://www.rewe.de/angebote/goettingen/241057/rewe-markt-reinhaeuser-landstr-177/"
+cookie_button = "#uc-center-container > div.sc-eBMEME.ixkACg > div > div.sc-jsJBEP.iXSECa > div > button:nth-child(2)"
+categories_button_css = string.Template("li.sos-navigation__categories-item:nth-child($num) > a:nth-child(1)")
 
 
 def scroll(page, js=False):
@@ -36,34 +37,44 @@ def scroll(page, js=False):
             break
 
 
+def create_page(playwright_context):
+    browser = playwright_context.firefox.launch(headless=False)
+    context = browser.new_context(viewport={'width': 1920, 'height': 1080})
+    return context.new_page()
+
+
+def click_cookie_banner(page):
+    page.wait_for_selector(cookie_button)
+    page.click(cookie_button)
+
+
+def cycle_categories(page):
+    for i in range(4, 16):
+        page.locator(categories_button_css.safe_substitute(num=i)).click()
+
+
+def get_products(html, css):
+    selector = parsel.Selector(text=html)
+    a_tags = selector.css(css)
+
+    return [
+        angebot.split(" €")[0].replace(",", "", 1)
+        for a_tag in a_tags
+        if (angebot := a_tag.attrib.get("aria-label"))
+        and angebot.find("€") > -1
+    ]
+
+
 def main():
-    angebote = []
-    with sync_playwright() as p:
-        browser = p.firefox.launch(headless=False)
-        context = browser.new_context(viewport={'width': 1920, 'height': 1080})
-        page = context.new_page()
-        page.goto(URL)
-
-        cookie_button = "#uc-center-container > div.sc-eBMEME.ixkACg > div > div.sc-jsJBEP.iXSECa > div > button:nth-child(2)"
-        page.wait_for_selector(cookie_button)
-        page.click(cookie_button)
-
-        for i in range(4, 16):
-            page.locator(button_css.safe_substitute(num=i)).click()
-
-        html = page.content()
-        selector = parsel.Selector(text=html)
-        a_tags = selector.css("a")
-
-        #IPython.embed()
-
-        angebote += [
-            angebot.split(" €")[0] for a_tag in a_tags
-            if (angebot := a_tag.attrib.get("aria-label"))
-            and angebot.find("€") > -1
-        ]
-        pp(angebote)
-        input()
+    with sync_playwright() as playwright_context:
+        page = create_page(playwright_context)
+        page.goto(url)
+        click_cookie_banner(page)
+        cycle_categories(page)
+        angebote = get_products(page.content(), "a")
+        #angebote = [product.replace(",", "", 1) for product in angebote]
+        with open("rewe_angebote.txt", "w") as rewe_file:
+            rewe_file.write("\n".join(angebote))
 
 
 if __name__ == '__main__':
